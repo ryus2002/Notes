a4b688a3474e6fe4c46cd98c1cdcfa2252f58b2b
ryus2002 <ryus2002@gmail.com>
2021-04-16 17:55:13 +0800

    Create v-model.php

---
+++ vue2/v-model.php
@@ -0,0 +1,55 @@
+<div class="wrapper" id="app">
+<form @submit.prevent="checkForm">
+  <p>
+    <auto-textarea v-model="info.activity_name"></auto-textarea>
+  </p>
+  <p>
+    <input type="submit" value="Submit">  
+  </p>
+</form>
+</div>
+
+<script>
+Vue.component('auto-textarea', {
+	props:['txt'],
+	data: function () {
+		return {
+      info: {
+        activity_name:"test",
+      },
+      //txt:"test_txt",
+		}
+	},
+	template: `
+	<div class="flex-grow-1">
+		<textarea>{{txt}}</textarea>
+	</div>
+	`,
+	methods: {
+	},
+	watch: {
+		txt: function() {
+			//this.countdown = this.length_count-this.txt.length
+		}
+	},
+	created() {
+		//this.countdown = this.length_count-this.txt.length
+	},
+});
+
+const app = new Vue({
+  el:'#app',
+  data:{
+    info: {
+      activity_name:null,
+    }
+  },
+  methods:{
+    checkForm:function(e) {
+      alert(this.info.activity_name);
+      alert(this.txt);
+      e.preventDefault();
+    }
+  }
+})
+</script>
